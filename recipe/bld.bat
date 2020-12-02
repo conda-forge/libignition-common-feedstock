42337d0dbb279b102cb14c643a594f285fdbc689
Silvio Traversaro <silvio@traversaro.it>
2020-12-02 21:59:00 +0100

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,6 +11,7 @@ cmake ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True ^
     -DCMAKE_CXX_STANDARD=17 ^
+    -DUSE_EXTERNAL_TINYXML2=ON ^
     %SRC_DIR%
 if errorlevel 1 exit 1
 
