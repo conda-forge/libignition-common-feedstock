d282016e9931bd5f1905917564a3b0b66dc474f7
Tobias Fischer <info@tobiasfischer.info>
2021-01-25 07:21:44 +1000

    Add CMAKE_ARGS to build.sh, disable tests on aarch/ppc

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,13 +3,22 @@
 mkdir build
 cd build
 
+if [ ${target_platform} == "linux-aarch64" ] || [ ${target_platform} == "linux-ppc64le" ]; then
+  # Disable tests
+  IGN_TEST_CMD=-DBUILD_TESTING:BOOL=OFF
+else
+  IGN_TEST_CMD=
+fi
+
 cmake .. \
       -G "Ninja" \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_PREFIX_PATH=$PREFIX -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
-      -DCMAKE_CXX_STANDARD=17
+      -DCMAKE_CXX_STANDARD=17 \
+      ${IGN_TEST_CMD} \
+      ${CMAKE_ARGS}
 
 cmake --build . --config Release
 cmake --build . --config Release --target install
