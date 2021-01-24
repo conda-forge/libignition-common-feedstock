35b4b12d9a12c64d01bcc52b6e90effa22d09933
Tobias Fischer <info@tobiasfischer.info>
2021-01-25 07:44:14 +1000

    Try avoiding try-run issues

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,8 +6,13 @@ cd build
 if [ ${target_platform} == "linux-aarch64" ] || [ ${target_platform} == "linux-ppc64le" ]; then
   # Disable tests
   IGN_TEST_CMD=-DBUILD_TESTING:BOOL=OFF
+  # Try-run doesn't work on cross-compile
+  IGN_TRY_RUN_CACHE=-DFREEIMAGE_RUNS:BOOL=ON
+  IGN_TRY_RUN_CACHE_ADVANCED=-DFREEIMAGE_RUNS__TRYRUN_OUTPUT=""
 else
   IGN_TEST_CMD=
+  IGN_TRY_RUN_CACHE=
+  IGN_TRY_RUN_CACHE_ADVANCED=
 fi
 
 cmake .. \
@@ -18,6 +23,8 @@ cmake .. \
       -DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=True \
       -DCMAKE_CXX_STANDARD=17 \
       ${IGN_TEST_CMD} \
+      ${IGN_TRY_RUN_CACHE} \
+      ${IGN_TRY_RUN_CACHE_ADVANCED} \
       ${CMAKE_ARGS}
 
 cmake --build . --config Release
